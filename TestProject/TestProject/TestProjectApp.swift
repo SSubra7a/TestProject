819f562ab91c831b8fc491ac8dbca3aa7140563d
Subrata Sutradhar <subrata.sutradhar@gmail.com>
2023-09-03 12:12:35 +0600

    added ios project

---
+++ TestProject/TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Subrata Sutradhar on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
